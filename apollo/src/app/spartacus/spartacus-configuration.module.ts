7b34e75d3db2d52807798d1227a7962f84eb6a1c
Maier <mma@sybit.de>
2021-08-05 16:59:59 +0200

    upgrade spartacus on apollo to spartacus 4.0

--- apollo/src/app/spartacus/spartacus-configuration.module.ts
+++ apollo/src/app/spartacus/spartacus-configuration.module.ts
@@ -3,15 +3,6 @@ import { translationChunksConfig, translations } from "@spartacus/assets";
 import { FeaturesConfig, I18nConfig, OccConfig, provideConfig, SiteContextConfig } from "@spartacus/core";
 import { defaultB2bCheckoutConfig, defaultB2bOccConfig } from "@spartacus/setup";
 import { defaultCmsContentProviders, layoutConfig, mediaConfig } from "@spartacus/storefront";
-import { SmartEditConfig } from "@spartacus/smartedit/root";
-
-export const defaultSmartEditConfig: SmartEditConfig = {
-   smartEdit: {
-      storefrontPreviewRoute: 'cx-preview',
-      allowOrigin: '*',
-   },
-};
-
 
 @NgModule({
   declarations: [],
@@ -27,7 +18,7 @@ export const defaultSmartEditConfig: SmartEditConfig = {
     context: {
       currency: ['USD','EUR'],
       language: ['en','de'],
-      baseSite: ['powertools-spa']
+	  baseSite: ['powertools-spa']
     },
   }), provideConfig(<I18nConfig>{
     i18n: {
@@ -35,17 +26,9 @@ export const defaultSmartEditConfig: SmartEditConfig = {
       chunks: translationChunksConfig,
       fallbackLang: 'en'
     },
-  }),
-  
-  provideConfig(<SmartEditConfig>{
-   smartEdit: {
-     allowOrigin: '*.sybit.de:443'
-   },
- }),
-  
-   provideConfig(<FeaturesConfig>{
+  }), provideConfig(<FeaturesConfig>{
     features: {
-      level: '3.3'
+      level: '4.0'
     }
   }), provideConfig(defaultB2bOccConfig), provideConfig(defaultB2bCheckoutConfig)]
 })
