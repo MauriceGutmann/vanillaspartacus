d2873fd539e2d8109ee03f77b3c12a8736f1fe78
Maurice Gutmann <Maurice.Gutmann@sybit.de>
2022-06-22 11:16:41 +0200

    new radio button 2

---
+++ apollo/src/app/spartacus/features/checkout/pay-with-crypto/pay-with-crypto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pay-with-crypto',
+  templateUrl: './pay-with-crypto.component.html',
+  styleUrls: ['./pay-with-crypto.component.scss']
+})
+export class PayWithCryptoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
